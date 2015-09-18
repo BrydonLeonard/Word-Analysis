22450571e8df529c3f50324e83ed83bd4bd33e73
Brydon Leonard <brydon.leonard@gmail.com>
2015-09-18 19:08:24 +0200

    Added script

---
+++ README.txt
@@ -0,0 +1,11 @@
+Basic script to count words in a whatsapp chat log.
+Get the chat logs from Whatsapp in Settings -> Chats and Calls -> Email chat
+Change "yourName" to your own name.
+
+Run the script as follows
+	run.py [chat file] [name for output]
+	eg. run.py chat.txt breakdown
+
+The output will be 2 csv files
+	1 with the breakdown of the entire conversation
+	1 with the breakdown of only your messages
---
+++ run.py
@@ -0,0 +1,71 @@
+import re
+import sys
+
+yourName = "Brydon Leonard"
+
+list = ''
+listYou = ''
+
+f = open(sys.argv[1], encoding='utf-8')
+
+for line in f:
+	try:
+		list += (re.sub('[^A-Za-z ]+', '', re.sub('^.+(?=-)-.+(?=:): ', '', line)).replace('<Media omitted>','').replace('\n',' ')+' ')
+		listYou += (re.sub('[^A-Za-z ]+', '', re.sub('^.+(?=-)-.+(?=:): ', '', re.sub('^.+(?=-)-.+(?=:)(?<!'+yourName+').+', '', line))).replace('<Media omitted>','').replace('\n',' ')+' ')
+	except:
+		pass
+
+
+def countingSortAndDisp(dataArr, labelArr, isBrydon):
+	if isBrydon == 1:
+		outputF = open(sys.argv[2]+'1way.csv','w')
+	else:
+		outputF = open(sys.argv[2]+'.csv', 'w')
+	max = 0
+	for i in dataArr:
+		if i > max:
+			max = i
+
+	tempData = []
+	tempLabel = []
+
+	for i in range(0,max+1):
+		tempData.append([])
+		tempLabel.append([])
+
+	for i in range(0, len(dataArr)):
+		if not labelArr[i] in [' ', '']:
+			tempLabel[dataArr[i]].append(labelArr[i])
+
+	for i in range(1, len(tempLabel)+1):
+		for j in range(0, len(tempLabel[len(tempLabel)-i])):
+			try:
+				outputF.write(tempLabel[len(tempLabel)-i][j] +',' + str(len(tempLabel)-i)+"\n")
+			except:
+				pass
+
+
+
+
+
+arr = list.lower().split(' ')
+freq = []
+words = []
+for item in arr:
+	if not item in words:
+		freq.append(1)
+		words.append(item)
+	else:
+		freq[words.index(item)] += 1
+countingSortAndDisp(freq, words,0)
+
+arr = listYou.lower().split(' ')
+freq = []
+words = []
+for item in arr:
+	if not item in words:
+		freq.append(1)
+		words.append(item)
+	else:
+		freq[words.index(item)] += 1
+countingSortAndDisp(freq, words, 1)
